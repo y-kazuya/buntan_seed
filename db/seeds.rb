0a767cf249b2fd214128a09d85b7c415c712e4e6
y-kazuya <y.kazuya.0124@au.com>
2018-12-17 12:04:39 +0900

    Tagを入力する際の候補表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+usages = %W[ 会議・研修場 写真撮影・ロケ撮影 イベント 演奏・パフォーマンス 個展・展覧会  商業施設 カフェ 公的施設 オフィス
+  宿泊施設 その他]
+
+usages.each { |a| Usage.create(name: a) }
+
+tags = %W[ 小学校跡 国道沿い 山奥 古民家 好立地 ]
+
+tags.each { |a| Tag.create(name: a, official: true)}
