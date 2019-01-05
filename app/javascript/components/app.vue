7934032ae4c013ab7c951846f9094f2fe50e6d50
Kyuma <hokumon2@icloud.com>
2019-01-05 23:56:52 +0900

    vueファイルの表示

---
+++ app/javascript/components/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>{{ name }}</p>
+    <ul>
+      <li v-for="location in locations" v-bind:key="location">
+        <span>{{ location }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      name: "kyuma",
+      locations: ["Canada", "Thailand", "Indonesia"]
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
