db15d2ee18d334fdcfb310181397fe0c19b693b4
y-kazuya <y.kazuya.0124@au.com>
2019-01-06 19:33:18 +0900

    エラー文の日本語化

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,12 @@ module BuntanSeed
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       %Q(#{html_tag}).html_safe
     end
+    config.i18n.default_locale = :ja
   end
 end
