3e3427df04232d307b1bf6bd698cb3a4b72b030e
y-kazuya <y.kazuya.0124@au.com>
2019-01-31 12:14:51 +0900

    ログイン修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
     # ユーザーのログインを確認する
     def logged_in_user
-      unless logged_in?
+      unless user_signed_in?
         store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
