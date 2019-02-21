a3cc6ab1a9ce66e45d9e82c311684ee8dbe7baa8
y-kazuya <y.kazuya.0124@au.com>
2019-02-21 14:36:54 +0900

    市町村取得apiの変更

--- app/assets/javascripts/getCity.js
+++ app/assets/javascripts/getCity.js
@@ -2,9 +2,9 @@ $(document).on('turbolinks:load', function(){
   const target = $(".select_citys")
 
   function replaceCitys(data){
-    data.data.forEach(function(city){
+    data.result.forEach(function(city){
 
-      let option = `<option value="${city.name}">${city.name}</option>`
+      let option = `<option value="${city.cityName}">${city.cityName}</option>`
       target.append(option)
     });
   }
@@ -19,28 +19,20 @@ $(document).on('turbolinks:load', function(){
     let state = Number($(".select_state").find('option:selected')[0].index)
     state += 1
 
-    if (state < 10) {
-      state = ("0"+state).slice(-2)
-    }
-
-    let url =`http://www.land.mlit.go.jp/webland/api/CitySearch?area=${state}`
-
-    // if (App.cable.url.match(/local/)) {
-    //   console.log(App.cable.url)
-    //   url = `http://www.land.mlit.go.jp/webland/api/CitySearch?area=${state}`
-    // }
-    // else{
-    //   console.log(App.cable.url)
-    //   url = `//www.land.mlit.go.jp/webland/api/CitySearch?area=${state}`
-    // }
+    let url = `https://opendata.resas-portal.go.jp/api/v1/cities?prefCode=${state}`
+    const api_key = "sLXRi2Ovt21lcBxeFykUus8r0XIiHIAqtPrxntoW"
 
     console.log(url)
     $.ajax({
+      headers: {
+        "X-API-KEY" : api_key
+       },
       url: url,
       type: "GET",
       dataType: 'json',
     })
     .done(function(data){
+      console.log(data)
       replaceCitys(data)
 
     })
