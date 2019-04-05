27ff35049301cd704f94b62363a7e7e20a9af049
Kyuma <hokumon2@icloud.com>
2019-04-05 17:36:48 +0900

    カテゴリを表示させた

--- app/javascript/components/item_detail.vue
+++ app/javascript/components/item_detail.vue
@@ -1,46 +1,47 @@
 <template>
   <div id="itempage">
-
-
     <!-- モバイル画面 -->
     <section id="item-mobile">
-      <b-card no-body
-            style="max-width: 100%;"
-            >
-        <b-carousel id="item-carousel"
-                    style="text-shadow: 1px 1px 2px #333; height: 373px;"
-                    controls
-                    indicators
-                    background="#ababab"
-                    :interval="8000"
-                    img-width="373"
-                    img-height="373"
-                    v-model="slide"
-                    @sliding-start="onSlideStart"
-                    @sliding-end="onSlideEnd"
-              >
-                <b-carousel-slide caption="スライド１"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド２"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド３"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド４"
-                                  text="スライドのキャッチコピー  "
-                                  img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png">
-                </b-carousel-slide>
-              </b-carousel>
+      <b-card no-body style="max-width: 100%;">
+        <b-carousel
+          id="item-carousel"
+          style="text-shadow: 1px 1px 2px #333; height: 373px;"
+          controls
+          indicators
+          background="#ababab"
+          :interval="8000"
+          img-width="373"
+          img-height="373"
+          v-model="slide"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+        >
+          <b-carousel-slide
+            caption="スライド１"
+            text="スライドのキャッチコピー"
+            img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="スライド２"
+            text="スライドのキャッチコピー"
+            img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="スライド３"
+            text="スライドのキャッチコピー"
+            img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png"
+          ></b-carousel-slide>
+          <b-carousel-slide
+            caption="スライド４"
+            text="スライドのキャッチコピー  "
+            img-src="https://cdn-ak.f.st-hatena.com/images/fotolife/k/kyuma-morita/20190202/20190202174926.png"
+          ></b-carousel-slide>
+        </b-carousel>
         <b-card-body>
-          <h4><strong>{{ item.title }}</strong></h4>
-          <p class="card-text">
-              {{ item.profile }}
-          </p>
+          <h4>
+            <strong>{{ item.title }}</strong>
+          </h4>
+          <p class="card-text">{{ item.profile }}</p>
         </b-card-body>
         <b-list-group flush>
           <b-list-group-item>
@@ -51,7 +52,7 @@
           <b-list-group-item>住所</b-list-group-item>
         </b-list-group>
         <div class="clearfix">
-          <b-img left src="https://picsum.photos/125/125/?image=58" alt="left image" />
+          <b-img left src="https://picsum.photos/125/125/?image=58" alt="left image"/>
           <div style="position: relative; left: 15px; top: 15px;">
             <h4>{{ item.user.name }}</h4>
             <p>{{ item.user.city }}</p>
@@ -60,19 +61,15 @@
           </div>
         </div>
         <b-card-body>
-            <a href="#"
-               class="card-link">シェアする</a>
-            <a href="#"
-               class="card-link">管理者にメッセージ</a>
+          <a href="#" class="card-link">シェアする</a>
+          <a href="#" class="card-link">管理者にメッセージ</a>
         </b-card-body>
         <b-card-footer>所在地</b-card-footer>
         <iframe src="https://maps.google.co.jp/maps?output=embed&q=原宿駅" class="map"></iframe>
       </b-card>
     </section>
 
-
     <!-- タブレット画面 -->
-
 
     <!-- PC画面 -->
     <section id="item-md">
@@ -81,62 +78,59 @@
           <b-col md="8">
             <b-row>
               <b-card-body>
-                <h4><strong>{{ item.title }}</strong></h4>
+                <h4>
+                  <strong>{{ item.title }}</strong>
+                </h4>
                 <b-badge variant="light">{{ item.user.city }}</b-badge>
-                <b-badge variant="secondary" v-if="category === 1">
-                  空き家
-                </b-badge>
-                <b-badge variant="secondary" v-else-if="category === 2">
-                  空き山
-                </b-badge>
-                <b-badge variant="secondary" v-else-if="category === 3">
-                  空き地
-                </b-badge>
-                <b-badge variant="secondary" v-else>
-                  カテゴリ不明
-                </b-badge>
+                <b-badge variant="secondary" v-if="category === 1">空き家</b-badge>
+                <b-badge variant="secondary" v-else-if="category === 2">空き山</b-badge>
+                <b-badge variant="secondary" v-else-if="category === 3">空き地</b-badge>
+                <b-badge variant="secondary" v-else>カテゴリ不明</b-badge>
               </b-card-body>
             </b-row>
             <b-row>
-              <b-carousel id="item-carousel"
-                    style="text-shadow: 1px 1px 2px #333;"
-                    controls
-                    indicators
-                    background="#ababab"
-                    :interval="4000"
-                    img-width="1024"
-                    img-height="480"
-                    v-model="slide"
-                    @sliding-start="onSlideStart"
-                    @sliding-end="onSlideEnd"
+              <b-carousel
+                id="item-carousel"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
               >
-                <b-carousel-slide caption="スライド１"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://picsum.photos/1024/480/?image=52">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド２"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://picsum.photos/1024/480/?image=52">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド３"
-                                  text="スライドのキャッチコピー"
-                                  img-src="https://picsum.photos/1024/480/?image=52">
-                </b-carousel-slide>
-                <b-carousel-slide caption="スライド４"
-                                  text="スライドのキャッチコピー  "
-                                  img-src="https://picsum.photos/1024/480/?image=52">
-                </b-carousel-slide>
+                <b-carousel-slide
+                  caption="スライド１"
+                  text="スライドのキャッチコピー"
+                  img-src="https://picsum.photos/1024/480/?image=52"
+                ></b-carousel-slide>
+                <b-carousel-slide
+                  caption="スライド２"
+                  text="スライドのキャッチコピー"
+                  img-src="https://picsum.photos/1024/480/?image=52"
+                ></b-carousel-slide>
+                <b-carousel-slide
+                  caption="スライド３"
+                  text="スライドのキャッチコピー"
+                  img-src="https://picsum.photos/1024/480/?image=52"
+                ></b-carousel-slide>
+                <b-carousel-slide
+                  caption="スライド４"
+                  text="スライドのキャッチコピー  "
+                  img-src="https://picsum.photos/1024/480/?image=52"
+                ></b-carousel-slide>
               </b-carousel>
             </b-row>
             <b-row>
               <b-card-body>
-                <p class="card-text">
-                  {{ item.profile }}
-                </p>
+                <p class="card-text">{{ item.profile }}</p>
                 <h5>設備・特徴</h5>
                 <ul>
-                  <li v-for="tag in tags" v-vind:key :key="tag">・{{tag}}</li>
-                </ul> 
+                  <li v-for="tag in tags" :key="tag.id">・{{tag.name}}</li>
+                </ul>
               </b-card-body>
             </b-row>
           </b-col>
@@ -172,7 +166,11 @@ export default {
         { isActive: true, table_left: "物件情報", table_right: "2LDK" },
         { isActive: true, table_left: "状態", table_right: "要改修" }
       ],
-      tags: ["Wifi", "水道", "駅近く", "空港近く", "海が近い", "川が近い"],
+      tags: [
+        { id: 1, name: "水道" },
+        { id: 2, name: "wifi" },
+        { id: 3, name: "トイレ" }
+      ],
       loading: false,
       item: "",
       error: null,
